dfe44a82b7dba7a939c1f878c6ee667574ff79ed
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-11 07:31:46 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootssedemo2/SpringbootSseDemo2Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootssedemo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSseDemo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSseDemo2Application.class, args);
+    }
+
+}
